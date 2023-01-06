2f1b2abcc34e17ca1eacad2584f2f4ff93c8d326
weiyuexin <3022422894@qq.com>
2023-01-06 21:45:59 +0800

    面向对象

---
+++ src/main/java/JavaEE/chapter04/Java03_Object.java
@@ -0,0 +1,33 @@
+package JavaEE.chapter04;
+
+/**
+ * @PackageName: JavaEE.chapter04
+ * @ProjectName: Java_atguigu
+ * @ClassName: Java03_Object
+ * @Author: Weiyuexin
+ * @Email: 3022422894@qq.com
+ * @Date: 2023/1/6 21:13
+ */
+public class Java03_Object {
+    public static void main(String[] args) {
+        // TODO 面向对象
+        /**
+         基本语法：
+                声明：
+                    class 类名{
+                        // 属性（0或多个）
+                        类型 名称 = 值；
+                        // 方法（行为 0或多个）
+                        void 方法名(){
+                            逻辑代码
+                        }
+                    }
+                使用：
+                    构建对象： new 类名()
+                    访问属性： 对象.属性
+                    访问方法： 对象.方法()
+
+
+         */
+    }
+}
